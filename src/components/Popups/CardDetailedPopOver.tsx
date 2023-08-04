677283ced7ed3304e814a1220c9cee9cf5b6945f
belkacem <belkacem.yerfa@gmail.com>
2023-08-04 14:49:52 +0100

    adding the description form for the cardPopup , with a custom scroll

--- src/components/Popups/CardDetailedPopOver.tsx
+++ src/components/Popups/CardDetailedPopOver.tsx
@@ -1,6 +1,8 @@
 import { Drawer } from "vaul";
 import Link from "next/link";
 import Image from "next/image";
+import { CardDescriptionForm } from "../forms/CardDescriptionForm";
+import { ScrollArea } from "../ui/scroll-area";
 
 type CardDetailedPopOverProps = {
   taskTitle: string;
@@ -14,39 +16,45 @@ export const CardDetailedPopOver = ({
       <Drawer.Trigger>{taskTitle}</Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40 " />
-        <Drawer.Content className="bg-white rounded-t-[10px] mt-24 fixed max-h-[80%] w-full bottom-0 left-0 right-0">
-          <div
-            className="p-5 bg-white h-full w-full md:w-[70%] m-auto
-          space-y-5 rounded-t-lg md:rounded-none "
-          >
-            <div className="max-w-full m-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300" />
-            <div className="w-full space-y-6 ">
-              <Image
-                src={
-                  "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
-                }
-                alt="pic"
-                height={200}
-                width={200}
-                className="w-full h-[200px] rounded-xl object-cover "
-                quality={100}
-              />
-              <div className="flex gap-x-5">
-                <div className="basis-3/4">
-                  <div className="space-y-2">
-                    <h2 className="text-base ">
-                      ✋🏿 Move anything that is actually started here
-                    </h2>
-                    <p className="text-xs text-[#BDBDBD] font-semibold">
-                      In list{" "}
-                      <span className="text-[#333333]">In Progress</span>
-                    </p>
+        <Drawer.Content className="bg-white rounded-t-[10px] mt-24 fixed h-[80%] w-full bottom-0 left-0 right-0 px-1">
+          <ScrollArea className="h-full w-full">
+            <div className="sticky top-0 max-w-full bg-white py-5 ">
+              <div className=" max-w-full m-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300" />
+            </div>
+            <div
+              className="px-4 pb-4 bg-white h-full w-full md:w-[70%] m-auto
+           rounded-t-lg md:rounded-none "
+            >
+              <div className="w-full space-y-6 ">
+                <Image
+                  src={
+                    "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
+                  }
+                  alt="pic"
+                  height={200}
+                  width={200}
+                  className="w-full h-[200px] rounded-xl object-cover "
+                  quality={100}
+                />
+                <div className="flex gap-x-5">
+                  <div className="basis-3/4 space-y-3 ">
+                    <div className="space-y-2">
+                      <Drawer.Title className="text-base ">
+                        ✋🏿 Move anything that is actually started here
+                      </Drawer.Title>
+                      <p className="text-xs text-[#BDBDBD] font-semibold">
+                        In list{" "}
+                        <span className="text-[#333333]">In Progress</span>
+                      </p>
+                    </div>
+                    <CardDescriptionForm />
+                    <div></div>
                   </div>
+                  <div className="basis-1/4">hi</div>
                 </div>
-                <div className="basis-1/4">hi</div>
               </div>
             </div>
-          </div>
+          </ScrollArea>
         </Drawer.Content>
       </Drawer.Portal>
     </Drawer.Root>
---
+++ src/components/forms/CardDescriptionForm.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { useState } from "react";
+import { Icons } from "../Icons";
+import { Button } from "../ui/button";
+import { Form, FormField, FormItem } from "../ui/form";
+import { Textarea } from "../ui/textarea";
+import { useForm } from "react-hook-form";
+import {
+  boardDescriptionSchema,
+  boardDescriptionSchemaType,
+} from "@/validation/board-description";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+export const CardDescriptionForm = () => {
+  const [value, setValue] = useState<string>("");
+  const [isDescriptionFormOpen, setIsDescriptionFormOpen] =
+    useState<boolean>(false);
+  const form = useForm<boardDescriptionSchemaType>({
+    resolver: zodResolver(boardDescriptionSchema),
+  });
+  const onSubmit = (data: boardDescriptionSchemaType) => {
+    console.log(data);
+    setValue(data.description ?? "");
+    setIsDescriptionFormOpen(false);
+  };
+  return (
+    <div className="space-y-3">
+      <div className="flex items-center gap-x-3">
+        <div className="text-[#BDBDBD] flex items-center gap-x-[6px]">
+          <Icons.File className="h-4 w-4" />
+          <h3 className="text-xs font-semibold">Description</h3>
+        </div>
+        {isDescriptionFormOpen ? null : (
+          <Button
+            className=" flex items-center gap-x-2 px-3 py-1 border border-[#BDBDBD] border-solid rounded-lg bg-transparent hover:bg-transparent text-[#828282]"
+            onClick={() => setIsDescriptionFormOpen(true)}
+          >
+            <Icons.Pencil className="h-4 w-4" />
+            Edit
+          </Button>
+        )}
+      </div>
+      <div className="">
+        {value && !isDescriptionFormOpen ? (
+          <p>{form.getValues("description")}</p>
+        ) : (
+          <Form {...form}>
+            <form
+              className="grid space-y-2 "
+              onSubmit={(...args) => void form.handleSubmit(onSubmit)(...args)}
+            >
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <Textarea
+                      placeholder={value ? value : "Description..."}
+                      className="resize-none h-[200px]"
+                      {...field}
+                    />
+                  </FormItem>
+                )}
+              />
+              <div className="space-x-2">
+                <Button
+                  type="submit"
+                  className="rounded-xl bg-[#219653] hover:bg-[#219653] py-1 px-3 disabled:bg-[#BDBDBD] disabled:cursor-not-allowed disabled:hover:bg-[#BDBDBD] disabled:opacity-70 text-sm"
+                  disabled={!form.formState.isValid}
+                >
+                  Save
+                </Button>
+                <Button
+                  type="button"
+                  className="rounded-xl text-[#828282] bg-transparent hover:bg-transparent py-1 px-3"
+                  onClick={() => setIsDescriptionFormOpen(false)}
+                >
+                  Cancel
+                </Button>
+              </div>
+            </form>
+          </Form>
+        )}
+      </div>
+    </div>
+  );
+};
